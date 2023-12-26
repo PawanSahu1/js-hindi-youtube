eafe0319645c4d57a0b277484250be57dfcabc7c
Pawan Sahu <96007722+PawanSahu1@users.noreply.github.com>
2023-12-26 07:06:36 +0000

    add new dataTypes file

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+//code readability should be high
+console.log(6+6)
+console.log("Pawan")
+
+let name = "pawan"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof"Pawan")
+console.log(typeof age)
+console.log(typeof null)//typeof null is object
+console.log(typeof undefined)
